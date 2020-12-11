60d6b0e9ee710c9c80ce284d9c2f7d9fa916cbcc
Ahmed Younes <cahmedc@gmail.com>
2020-12-11 09:00:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,91 +1,113 @@
-import React, { Component ,  } from 'react'
-import Form from './Components/Form/index';
-import List from './Components/List/index';
-import Color from './Components/colors/index'
-import './App.css'
-
+import React, { Component } from "react";
+import Form from "./Components/Form/index";
+import List from "./Components/List/index";
+import Color from "./Components/colors/index";
+import "./App.css";
 
 export default class App extends Component {
-  
   componentDidMount() {
-    console.log('bhbbbbvbb')
-    this.addCourse()
+    console.log("bhbbbbvbb");
+    this.addCourse();
   }
   state = {
-    courses : [
-      { name : "html"},
-      { name : "css"},
-      { name : "js"},
-      { name : "php"},
-      
+    courses: [
+      { name: "html" },
+      { name: "css" },
+      { name: "js" },
+      { name: "php" },
     ],
-    NewCourse : ''
-  }
+    NewCourse: "",
+  };
 
-  
-
-  //UbdateCourse 
-  updateCourse =(e) => {
+  //UbdateCourse
+  updateCourse = (e) => {
     this.setState({
-      NewCourse:e.target.value
-    })
-  }
-  
+      NewCourse: e.target.value,
+    });
+  };
+
   //Add Course
-  addCourse = () =>{
+  addCourse = () => {
+    console.log(this.state.NewCourse);
     // e.preventDefault();
-    let {NewCourse} = this.state;
-    let {courses} = this.state
-    courses.push({name:NewCourse})
-    if("" === ""){
-      return<p> you should enter information</p>
-    }else{
-      // this.props.addCourse()
+    // let { NewCourse, courses } = this.state;
+    if (this.state.NewCourse !== "") {
       this.setState({
-        NewCourse:''
-    })
+        courses: [...this.state.courses, { name: this.state.NewCourse }],
+      });
     }
-    }
-  
+    this.setState({
+      NewCourse: "",
+    });
+    // courses.push({ name: NewCourse });
+    // if ("" === "") {
+    //   return <p> you should enter information</p>;
+    // } else {
+    //   // this.props.addCourse()
+    //   this.setState({
+    //     NewCourse: "",
+    //   });
+    // }
+  };
+
   //DelteCourse
-  deleteCourse = (id) =>{
-    console.log(id)
-     let {courses} = this.state;
-     courses.splice(id , 1);
-     this.setState({
-       courses
-      })
-    }
-    //  EditeCoures
-    EditeCoures = (id , value) => {
-    let {courses} = this.state;
+  deleteCourse = (id) => {
+    console.log(id);
+    let { courses } = this.state;
+    courses.splice(id, 1);
+    this.setState({
+      courses,
+    });
+  };
+  //  EditeCoures
+  EditeCoures = (id, value) => {
+    let { courses } = this.state;
     let course = courses[id];
-    course['name'] = value;
-    this.setState ({
-      courses
-    })
-}
+    course["name"] = value;
+    this.setState({
+      courses,
+    });
+  };
 
   render() {
-    const {courses} = this.state;
-    let {length} = courses;
-    const courseList = length ?(
-      courses.map((course , id) => {
-
-        return <List componentDidMount={this.componentDidMount} info={course}key={id} update={this.handleChange} id={id} Delete={this.deleteCourse} editeCoures={this.EditeCoures}addCourse={this.addCourse} />
-    })
-    ):(
+    const { courses } = this.state;
+    let { length } = courses;
+    const courseList = length ? (
+      courses.map((course, id) => {
+        console.log(course);
+        return (
+          <List
+            componentDidMount={this.componentDidMount}
+            info={course}
+            key={id}
+            update={this.handleChange}
+            id={id}
+            Delete={this.deleteCourse}
+            editeCoures={this.EditeCoures}
+            addCourse={this.addCourse}
+          />
+        );
+      })
+    ) : (
       <p>there is not course to show </p>
-    )
-   
+    );
+
     return (
       <div className="App">
         <div className="title"> Welcome To My Page</div>
         <Color />
         <h1>Add course</h1>
-        <Form NullInput={this.state.NewCourse} Update={this.updateCourse} addCourse={this.addCourse} componentDidMount={this.componentDidMount} />
+        <Form
+          nullInput={this.state.NewCourse}
+          update={this.updateCourse}
+          addCourse={(e) => {
+            e.preventDefault();
+            this.addCourse();
+          }}
+          componentDidMount={this.componentDidMount}
+        />
         <ul>{courseList}</ul>
       </div>
-    )
+    );
   }
 }
--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,59 +1,74 @@
-import React , {Component,Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
 export default class List extends Component {
-   
-
-    state = {
-        Edite : false
-    }
+  state = {
+    Edite: false,
+  };
 
   renderCoures = () => {
-      return(
-        <li>
-            <span>{this.props.info.name}</span>
-            <button onClick={() => {this.toggleState()}}>Edite Course</button>
-            <button onClick={() =>{this.props.Delete(this.props.id)}}> Delete Course</button>
+    return (
+      <li>
+        <span>{this.props.info.name}</span>
+        <button
+          onClick={() => {
+            this.toggleState();
+          }}
+        >
+          Edite Course
+        </button>
+        <button
+          onClick={() => {
+            this.props.Delete(this.props.id);
+          }}
+        >
+          {" "}
+          Delete Course
+        </button>
       </li>
-      )
-  }
-  focusMethod = function getFocus() {           
+    );
+  };
+  focusMethod = function getFocus() {
     document.getElementById().focus();
-  }
-  
-  // Toggle State 
+  };
+
+  // Toggle State
   toggleState = () => {
-      let {Edite} = this.state ;
-      this.setState({
-        Edite: !Edite
-      })
-  }
+    let { Edite } = this.state;
+    this.setState({
+      Edite: !Edite,
+    });
+  };
 
   updateCouresItem = (e) => {
-      e.preventDefault();
-      this.props.editeCoures(this.props.id , this.input.value);
-      this.toggleState();
+    e.preventDefault();
+    this.props.editeCoures(this.props.id, this.input.value);
+    this.toggleState();
+  };
+  //    handleFocus(){
+  //     searchInput.current.focus()
+  //   }
 
-  } 
-//    handleFocus(){
-//     searchInput.current.focus()
-//   }
-  
   //render Update Form
-   renderUpdateForm = () =>{
-
-    return(
-        <form onSubmit={this.updateCouresItem}>
-            <input  type="text" ref={(v) => {this.input = v}} defaultValue={this.props.info.name} />
-            <button >Update Course</button>
-        </form>
-    )
-   }
-    render(){
-        let {Edite} = this.state;
-        return(
-            <Fragment>
-              { Edite ? this.renderUpdateForm(): this.renderCoures()}
-            </Fragment>
-        )
-    }
+  renderUpdateForm = () => {
+    return (
+      <form onSubmit={this.updateCouresItem}>
+        <input
+          type="text"
+          ref={(v) => {
+            this.input = v;
+          }}
+          defaultValue={this.props.info.name}
+        />
+        <button>Update Course</button>
+      </form>
+    );
+  };
+  render() {
+    let { Edite } = this.state;
+    return (
+      <Fragment>
+        {Edite ? this.renderUpdateForm() : this.renderCoures()}
+      </Fragment>
+    );
+  }
 }
